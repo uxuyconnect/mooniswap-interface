5251bfe1051d0cf77b2de647586af4bc5e5faa88
asimbirskiy <73834656+asimbirskiy@users.noreply.github.com>
2020-12-16 12:42:17 +0300

    Updated default token list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -157,6 +157,6 @@ export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.Big
 
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gateway.ipfs.io/ipfs/QmYp1bsE1vtyksTPwSG1nHkYxBhRyx1eiZyXz7oLfkFeHP'
+  'https://gateway.ipfs.io/ipfs/QmULkWdsKeFEVQFWrbJcCCmkuhSdTmN3WjHc75tRqV22qK'
 
 export const REFERRAL_ADDRESS_STORAGE_KEY = 'referral-address'
